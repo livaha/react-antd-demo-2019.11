13d41f0e61084ad15f8cd88555955fa3faffcfb1
livaha； <1192137406@qq.com>
2019-11-29 17:11:13 +0800

    add docs folder for github pages

---
+++ docs/precache-manifest.312d368ac94cec69060f2a8c33d948f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7dab76a7c4304e8bb61347e90bcfca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a70cead09e88fe775ea",
+    "url": "/static/css/2.8a247e45.chunk.css"
+  },
+  {
+    "revision": "c9dc6d0c678619a5a02e",
+    "url": "/static/css/main.10088cef.chunk.css"
+  },
+  {
+    "revision": "1a70cead09e88fe775ea",
+    "url": "/static/js/2.b1e892c6.chunk.js"
+  },
+  {
+    "revision": "c9dc6d0c678619a5a02e",
+    "url": "/static/js/main.1a5187ad.chunk.js"
+  },
+  {
+    "revision": "a7927b55037a4ea9d056",
+    "url": "/static/js/runtime-main.b2df4a99.js"
+  }
+]);
